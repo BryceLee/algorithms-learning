8502b9eb041f98d93927b1ac913cbab3709e040c
lizhongxin <leezx1993@163.com>
2020-04-24 10:48:15 +0800

    Correct READ.md

--- leetcode/interviewBiNode.java
+++ leetcode/interviewBiNode.java
@@ -30,7 +30,7 @@ class Solution {
             return;
         }
         centerNode(root.left);
-        //prev.left=null; !!! error?
+        //prev.left=null; 不要这样的代码！prev.right.left=null,其实就是root.left=null
         root.left=null;
         prev.right=root;
         prev=root;
@@ -41,24 +41,25 @@ class Solution {
 class Solution {
     public TreeNode convertBiNode(TreeNode root) {
         // 1，画图分析可以发现，需要对原树左中序遍历
-        // 2，如何保证原址呢？先用一个链表把链表存下来. 利用
+        // 2，如何保证原址呢？
+        //  ---->先用一个链表把链表存下来. 可是不知道前驱节点，显示不行，还是选择递归
         LinkedList<TreeNode> linkedlist = new LinkedList<>();
-        getLinkedList(list, root);
-        while (!linkedlist.isEmpty()) {
-            TreeNode p = linkedlist.poll();
-            root=p.val;
-            root.left=null;
-            root.right=p
-        }
+        // getLinkedList(list, root);
+        // while (!linkedlist.isEmpty()) {
+        //     TreeNode p = linkedlist.poll();
+        //     root=p.val;
+        //     root.left=null;
+        //     root.right=p
+        // }
     }
 
-    public void getLinkedList(LinkedList<TreeNode> list, TreeNode root) {
-        if (root == null) {
-            return;
-        }
-        getLinkedList(list, root.left);
-        list.add(root);
-        getLinkedList(list, root.right);
-    }
+    // public void getLinkedList(LinkedList<TreeNode> list, TreeNode root) {
+    //     if (root == null) {
+    //         return;
+    //     }
+    //     getLinkedList(list, root.left);
+    //     list.add(root);
+    //     getLinkedList(list, root.right);
+    // }
 
 }
